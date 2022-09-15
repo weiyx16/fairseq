a19f4f08c79491f606301b69f4d1759bcf456307
weiyx <weason1998@gmail.com>
2022-09-15 11:24:51 +0800

    seperate port for multi_ft; maybe key for amlt

--- fairseq_cli/multi_ft.py
+++ fairseq_cli/multi_ft.py
@@ -6,19 +6,19 @@ data_path = os.environ.get('data_path')
 pre_model = os.environ.get('pre_model')
 def train(is_end, gpu_id):
     if gpu_id == 0:
-        os.system(f'python single_ft.py --config-name mnli --task-name MNLI --data-path {data_path} --ckpt {pre_model} --device-id 0 --run-time 5')
+        os.system(f'MASTER_PORT=12345 python single_ft.py --config-name mnli --task-name MNLI --data-path {data_path} --ckpt {pre_model} --device-id 0 --run-time 5')
         with is_end.get_lock():
             is_end.value += 1
     elif gpu_id == 1:
-        os.system(f'python single_ft.py --config-name qqp --task-name QQP --data-path {data_path} --ckpt {pre_model} --device-id 1 --run-time 5')
+        os.system(f'MASTER_PORT=12346 python single_ft.py --config-name qqp --task-name QQP --data-path {data_path} --ckpt {pre_model} --device-id 1 --run-time 5')
         with is_end.get_lock():
             is_end.value += 1
     elif gpu_id == 2:
-        os.system(f'python single_ft.py --config-name qnli --task-name QNLI --data-path {data_path} --ckpt {pre_model} --device-id 2 --run-time 5')
+        os.system(f'MASTER_PORT=12347 python single_ft.py --config-name qnli --task-name QNLI --data-path {data_path} --ckpt {pre_model} --device-id 2 --run-time 5')
         with is_end.get_lock():
             is_end.value += 1
     elif gpu_id == 3:
-        os.system(f'python single_ft.py --config-name sst_2 --task-name SST-2 --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5; python single_ft.py --config-name cola --task-name CoLA --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5; python single_ft.py --config-name mrpc --task-name MRPC --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5; python single_ft.py --config-name sts_b --task-name STS-B --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5; python single_ft.py --config-name rte --task-name RTE --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5;')
+        os.system(f'MASTER_PORT=12348 python single_ft.py --config-name sst_2 --task-name SST-2 --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5;MASTER_PORT=12348 python single_ft.py --config-name cola --task-name CoLA --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5;MASTER_PORT=12348 python single_ft.py --config-name mrpc --task-name MRPC --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5;MASTER_PORT=12348 python single_ft.py --config-name sts_b --task-name STS-B --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5;MASTER_PORT=12348 python single_ft.py --config-name rte --task-name RTE --data-path {data_path} --ckpt {pre_model} --device-id 3 --run-time 5;')
         with is_end.get_lock():
             is_end.value += 1
 
